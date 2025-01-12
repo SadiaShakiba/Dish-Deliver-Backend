950ce9af380b3cfc837457b3f24b2ba6e0260757
SadiaShakiba <sakiba26999@gmail.com>
2025-01-12 15:56:36 +0600

    backend: cartItem entity getter/setter methods added

--- src/main/java/com/sadiaBhuiyan/DishDeliver/Backend/Model/CartItem.java
+++ src/main/java/com/sadiaBhuiyan/DishDeliver/Backend/Model/CartItem.java
@@ -30,4 +30,66 @@ public class CartItem {
     private List<String> ingredients;
 
     private Long totalPrice;
+
+    public CartItem() {
+    }
+
+    // Parameterized Constructor
+    public CartItem(Long id, Cart cart, Food food, int quantity, List<String> ingredients, Long totalPrice) {
+        this.id = id;
+        this.cart = cart;
+        this.food = food;
+        this.quantity = quantity;
+        this.ingredients = ingredients;
+        this.totalPrice = totalPrice;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Long totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
